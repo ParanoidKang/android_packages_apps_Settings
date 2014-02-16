e4a7a6ea0e3048d9ed10b3530b21fdebb221f51c
xilence2210 <xilence2210@gmail.com>
2014-02-16 20:53:34 +0800

    Lockscreen notification (2/2)
    Change-Id: Iedcf9546c05c9faba7ce387d77a1e277ff442588

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -80,7 +80,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mScreenTimeoutPreference;
     private ListPreference mStatusBarTraffic;
     private Preference mScreenSaverPreference;
-    private PreferenceScreen mBatteryPulse;
 
     private CheckBoxPreference mAdaptiveBacklight;
     private CheckBoxPreference mTapToWake;
@@ -100,8 +99,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.display_settings);
         
-        Resources res = getResources();
-
         Resources res = getResources();
 
         mAccelerometer = (CheckBoxPreference) findPreference(KEY_ACCELEROMETER);
